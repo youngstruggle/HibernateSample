fecb31ba652f3774b56cd9334a59b6d945caf656
youngstruggle <febrian.nana@gmail.com>
2018-10-13 20:19:04 +0700

    New Commit

--- ProteinTracker/src/main/java/com/simpeprogrammer/MainClass.java
+++ ProteinTracker/src/main/java/com/simpeprogrammer/MainClass.java
@@ -6,6 +6,15 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		Session session = HibernateUtilities.getSessionFactory().openSession();
+		session.beginTransaction();
+
+		User user = new User();
+		user.setName("Joe");
+		user.setGoal(250);
+		session.save(user);
+
+		session.getTransaction().commit();
 		session.close();
+		HibernateUtilities.getSessionFactory().close();
 	}
 }
